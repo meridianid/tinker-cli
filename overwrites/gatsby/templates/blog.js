41bec39b959ad9c49456f0f9936100c216c492fa
Ongki Herlambang <meepomodeon@gmail.com>
2019-07-08 01:11:38 +0700

    update gatsby templates

--- overwrites/gatsby/templates/blog.js
+++ overwrites/gatsby/templates/blog.js
@@ -1,33 +1,49 @@
 import React from 'react'
+import cx from 'classnames'
+import styles from './blog.module.scss'
 import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs';
+
+import Default from './../layouts/Default/Default'
+import Container from '../layouts/Container/Container';
+import Footer from '../components/Footer/Footer';
+import Text from '../components/Text/Text';
+import linkResolver from './../utils/prismic/linkResolver'
+import htmlSerializer from './../utils/prismic/htmlSerializer'
 
 const Post = ({data}) => {
-  let blog = data.prismicBlogs.data
-  console.log('DATA: ', blog)
+  let content = data.prismic
   return (
-    <main>
-      <h1>{blog.title.text}</h1>
-    </main>
+    <Default>
+      <Container post component="section" className={cx(styles.content)}>
+        <header className={styles.header}>
+          <Text heading1 className={styles.heading1} component="h1">{content.blogs.title[0].text}</Text>
+        </header>
+        <main>
+          <RichText htmlSerializer={htmlSerializer} render={content.blogs.body}></RichText>
+        </main>
+      </Container>
+      <Footer />
+    </Default>
   )
 }
 
 export default Post
 
 export const pageQuery = graphql`
-  query PostBySlug($uid: String!) {
-    prismicBlogs(uid: { eq: $uid }) {
-      id
-      data {
-        title {
-          text
+  query PostById($lang: String!, $uid: String!) {
+    prismic {
+      blogs(uid: $uid, lang: $lang) {
+        _meta {
+          id
+          uid
         }
-        short_excerpt {
-          text
-        }
+        title
+        cover
+        short_excerpt
+        published_date
         keywords
-        body {
-          html
-        }
+        body
       }
     }
   }
