ee4e0a874cdaf0ee4ecf45a7e7a331e3d202f334
Ongki Herlambang <meepomodeon@gmail.com>
2019-08-11 04:16:04 +0700

    refine gatsby <Default/>

--- overwrites/gatsby/Default/Default.js
+++ overwrites/gatsby/Default/Default.js
@@ -1,32 +1,22 @@
 import styles from './Default.module.scss'
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
 import "./Default.module.scss"
-import './../../styles/invoker-layout/grid.module.scss'
 import '../../assets/fonts/fonts'
+
+/**
+ * Uncomment below lines if you want to use legacy grid and column using Bootstrap grid
+ */
+// import './../../styles/invoker-layout/grid.module.scss'
 
 import Navbar from "../../components/Navbar/Navbar"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <main className={styles.root}>
-        <Navbar bleed/>
-        {children}
-      </main>
-    )}
-  />
+  <main className={styles.root}>
+    <Navbar bleed />
+    {children}
+  </main>
 )
 
 Layout.propTypes = {
